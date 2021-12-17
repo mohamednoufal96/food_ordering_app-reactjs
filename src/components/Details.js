f8edfa1e7129b5a49f7505822b4c2d0b8861ad37
Mohamed Noufal <getmohamednoufal@gmail.com>
2021-12-17 15:04:50 +0530

    production url added

--- src/components/Details.js
+++ src/components/Details.js
@@ -162,136 +162,142 @@ export default class Details extends Component {
     render() {
         const { restaurant, menu, isMenuOpen, totalPrice } = this.state;
         return (
-            
-            <div className="details container">
-                {restaurant ? (
-                    <>
-                        <div className="images">
-                            <Carousel showThumbs={false}>
-                                {restaurant.thumb.map((item, index) => {
-                                    return (
-                                        <div>
-                                            <img
-                                                className="carousel-img"
-                                                key={index}
-                                                src={require(`../${item}`)}
-                                                alt="not found"
-                                            />
-                                        </div>
-                                    );
-                                })}
-                            </Carousel>
-                        </div>
-
-                        <h1 className="restaurant-name mt-5">{restaurant.name}</h1>
-                        <div className="button-container d-flex justify-content-end">
-                            <button className="btn btn-danger" onClick={this.openMenu}>
-                                Place Online Order
-                            </button>
-                        </div>
-                        <div className="tabs-container">
-                            <Tabs>
-                                <TabList>
-                                    <Tab>Overview</Tab>
-                                    <Tab>Contact</Tab>
-                                </TabList>
-
-                                <TabPanel>
-                                    <h2 className="title my-4">About this place</h2>
-
-                                    <div className="my-4">
-                                        <p className="desc-title mb-2 ">Cuisine</p>
-                                        {restaurant.cuisine.map((item, index) => {
-                                            return (
-                                                <p className="desc-text " index={index}>
-                                                    {item.name}
-                                                </p>
-                                            );
-                                        })}
-                                    </div>
-
-                                    <div className="my-4">
-                                        <p className="desc-title mb-2 ">Average Cost</p>
-                                        <p className="desc-text ">₹ {restaurant.min_price} for two people (approx.)</p>
-                                    </div>
-                                </TabPanel>
-                                <TabPanel>
-                                    <div className="my-4">
-                                        <p className="phone-title mb-2">Phone Number</p>
-                                        <p className="phone">+{restaurant.contact_number} </p>
-                                    </div>
-                                    <div className="my-4">
-                                        <p className="address-title mb-2">{restaurant.name}</p>
-                                        <p className="address-text">
-                                            {restaurant.locality}
-                                            <br />
-                                            {restaurant.city}
-                                        </p>
-                                    </div>
-                                </TabPanel>
-                            </Tabs>
-                        </div>
-                        <Modal isOpen={isMenuOpen}>
-                            <div className="modal-content">
-                                <div className="d-flex justify-content-between align-items-center my-2">
-                                    <h2 className="heading">Menu</h2>
-                                    <h2 className="d-flex justify-content-end">
-                                        <button
-                                            className="close-button btn btn-outline-danger justify-self-end"
-                                            onClick={this.closeMenu}
-                                        >
-                                            X
-                                        </button>
-                                    </h2>
-                                </div>
-                                <h4 className="restaurant-name my-1">{restaurant.name}</h4>
-                                <ul className="menu">
-                                    {menu
-                                        ? menu.map((item, index) => {
-                                              return (
-                                                  <li key={index} className="menu-item my-2">
-                                                      <div className="row">
-                                                          <div className="col-8">
-                                                              <div className="item-info item-name">{item.itemName}</div>
-
-                                                              {item.isVeg ? (
-                                                                  <div className="item-info text-success">Veg</div>
-                                                              ) : (
-                                                                  <div className=" item-info text-danger ">Non-veg</div>
-                                                              )}
-                                                              <div className="item-info">{item.itemDescription}</div>
-                                                          </div>
-
-                                                          <div className="item-info col-2">&#8377; {item.itemPrice}</div>
-                                                          <div className="col-2">
-                                                              <button
-                                                                  className="add-button btn btn-light"
-                                                                  onClick={() => this.addItemHandler(item.itemPrice)}
-                                                              >
-                                                                  Add
-                                                              </button>
-                                                          </div>
-                                                      </div>
-                                                  </li>
-                                              );
-                                          })
-                                        : null}
-                                </ul>
-
-                                <div className="my-3 d-flex justify-content-between align-items-center my-2">
-                                    <h4 className="subtotal">Subtotal : &#8377; {totalPrice}</h4>
-                                    <button className="payNow-button btn btn-success" onClick={() => this.paymentHandler()}>
-                                        Pay now
-                                    </button>
-                                </div>
+            <div className="detailsPage-container">
+                <div className="details container">
+                    {restaurant ? (
+                        <>
+                            <div className="images">
+                                <Carousel showThumbs={false}>
+                                    {restaurant.thumb.map((item, index) => {
+                                        return (
+                                            <div>
+                                                <img
+                                                    className="carousel-img"
+                                                    key={index}
+                                                    src={require(`../${item}`)}
+                                                    alt="not found"
+                                                />
+                                            </div>
+                                        );
+                                    })}
+                                </Carousel>
                             </div>
-                        </Modal>
-                    </>
-                ) : (
-                    <div>
-                        <h3 className="loading text-center">Loading.... Please wait</h3>
-                    </div>
-                )}
+
+                            <h1 className="restaurant-name mt-5">{restaurant.name}</h1>
+                            <div className="button-container d-flex justify-content-end">
+                                <button className="btn btn-danger" onClick={this.openMenu}>
+                                    Place Online Order
+                                </button>
+                            </div>
+                            <div className="tabs-container">
+                                <Tabs>
+                                    <TabList>
+                                        <Tab>Overview</Tab>
+                                        <Tab>Contact</Tab>
+                                    </TabList>
+
+                                    <TabPanel>
+                                        <h2 className="title my-4">About this place</h2>
+
+                                        <div className="my-4">
+                                            <p className="desc-title mb-2 ">Cuisine</p>
+                                            {restaurant.cuisine.map((item, index) => {
+                                                return (
+                                                    <p className="desc-text " index={index}>
+                                                        {item.name}
+                                                    </p>
+                                                );
+                                            })}
+                                        </div>
+
+                                        <div className="my-4">
+                                            <p className="desc-title mb-2 ">Average Cost</p>
+                                            <p className="desc-text ">₹ {restaurant.min_price} for two people (approx.)</p>
+                                        </div>
+                                    </TabPanel>
+                                    <TabPanel>
+                                        <div className="my-4">
+                                            <p className="phone-title mb-2">Phone Number</p>
+                                            <p className="phone">+{restaurant.contact_number} </p>
+                                        </div>
+                                        <div className="my-4">
+                                            <p className="address-title mb-2">{restaurant.name}</p>
+                                            <p className="address-text">
+                                                {restaurant.locality}
+                                                <br />
+                                                {restaurant.city}
+                                            </p>
+                                        </div>
+                                    </TabPanel>
+                                </Tabs>
+                            </div>
+                            <Modal isOpen={isMenuOpen}>
+                                <div className="modal-content">
+                                    <div className="d-flex justify-content-between align-items-center my-2">
+                                        <h2 className="heading">Menu</h2>
+                                        <h2 className="d-flex justify-content-end">
+                                            <button
+                                                className="close-button btn btn-outline-danger justify-self-end"
+                                                onClick={this.closeMenu}
+                                            >
+                                                X
+                                            </button>
+                                        </h2>
+                                    </div>
+                                    <h4 className="restaurant-name my-1">{restaurant.name}</h4>
+                                    <ul className="menu">
+                                        {menu
+                                            ? menu.map((item, index) => {
+                                                  return (
+                                                      <li key={index} className="menu-item my-2">
+                                                          <div className="row">
+                                                              <div className="col-8">
+                                                                  <div className="item-info item-name">{item.itemName}</div>
+
+                                                                  {item.isVeg ? (
+                                                                      <div className="item-info text-success">Veg</div>
+                                                                  ) : (
+                                                                      <div className=" item-info text-danger ">Non-veg</div>
+                                                                  )}
+                                                                  <div className="item-info">{item.itemDescription}</div>
+                                                              </div>
+
+                                                              <div className="item-info col-2">
+                                                                  &#8377; {item.itemPrice}
+                                                              </div>
+                                                              <div className="col-2">
+                                                                  <button
+                                                                      className="add-button btn btn-light"
+                                                                      onClick={() => this.addItemHandler(item.itemPrice)}
+                                                                  >
+                                                                      Add
+                                                                  </button>
+                                                              </div>
+                                                          </div>
+                                                      </li>
+                                                  );
+                                              })
+                                            : null}
+                                    </ul>
+
+                                    <div className="my-3 d-flex justify-content-between align-items-center my-2">
+                                        <h4 className="subtotal">Subtotal : &#8377; {totalPrice}</h4>
+                                        <button
+                                            className="payNow-button btn btn-success"
+                                            onClick={() => this.paymentHandler()}
+                                        >
+                                            Pay now
+                                        </button>
+                                    </div>
+                                </div>
+                            </Modal>
+                        </>
+                    ) : (
+                        <div>
+                            <h3 className="loading text-center">Loading.... Please wait</h3>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
