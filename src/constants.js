400d3eaebd7ad5da6a47c5d9f16523f2ddc9eed0
Mohamed Noufal <getmohamednoufal@gmail.com>
2021-12-17 16:04:19 +0530

    error fixed in constants file

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const DEVELOPMENT_URL = "http://localhost:5454";
-const PRODUCTION_URL = " https://zomato-clone-be.herokuapp.com";
+const PRODUCTION_URL = "https://zomato-clone-be.herokuapp.com";
 
 exports.API_URL = PRODUCTION_URL;
