e264a1dadeaef5a2a681d524038af18c51794ea9
Mohamed Noufal <getmohamednoufal@gmail.com>
2021-12-20 12:25:29 +0530

    port no. and error in production url fixed in constants file

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const DEVELOPMENT_URL = "http://localhost:5454";
-const PRODUCTION_URL = "https://zomato-clone-be.herokuapp.comhello";
+const DEVELOPMENT_URL = "http://localhost:5000";
+const PRODUCTION_URL = "https://zomato-clone-be.herokuapp.com";
 
 exports.API_URL = PRODUCTION_URL;
